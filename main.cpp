5e7192554556ebc4268abf25aa89f8173d2a462f
gbartl <george.bartlett1991@gmail.com>
2016-08-21 22:52:26 +0100

    main renaming

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
 #include <dlfcn.h>
 
@@ -12,18 +13,20 @@ using std::endl;
 
 int main(int argc, char * argv[]) {
     int retVal = 1;
+
     ProcessorLoader processorLoader;
 
-    bool processorsOK = true;
+    bool libsOK = true;
     for (int i=1; i<argc; ++i) {
-        bool processorLoaded = processorLoader.openLib(argv[i]);
-        if (processorLoaded == false) {
-            processorsOK = false;
+        bool libOpenSuccess = processorLoader.openLib(argv[i]);
+        if (libOpenSuccess == false) {
+            libsOK = false;
         }
     }
 
-    if (processorsOK == true) {
+    if (libsOK == true) {        
         vector<Processor*> processors = processorLoader.createProcessingChainInstance();
+        
         uint8_t data[]= {0,1,2,3,4};
         for (int i=0; i<sizeof(data); ++i) {
             cout<<(int)data[i]<<" ";
@@ -36,9 +39,8 @@ int main(int argc, char * argv[]) {
         for (int i=0; i<sizeof(data); ++i) {
             cout<<(int)data[i]<<" ";
         }
-        cout<<endl;
-        retVal = 0;
+        cout<<endl;     
     }
 
-    return retVal;
+    return 0;
 };
